79628bca41cbc26e2b1ed786e31161962597dc6f
Timothy Njoroge <timothynjoroge56@gmail.com>
2023-05-03 18:40:38 +0300

    - Add relationships between tables - Insert new data - Query data using select comand and join using related tables

--- queries.sql
+++ queries.sql
@@ -72,3 +72,33 @@ SELECT AVG(weight_kg) as avg_weight FROM animals;
 SELECT neutered, avg(escape_attempts) as escapes FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE dob >= '1990-01-01' AND dob <= '2000-12-31' GROUP BY species;
+
+/* Task Three */
+SELECT full_name as owner_full_name, name as animal_name FROM OWNERS 
+  JOIN ANIMALS ON owners.id = animals.owner_id 
+  WHERE full_name='Melody Pond'; 
+
+SELECT specie_name, name as animal_name FROM SPECIES 
+  JOIN ANIMALS ON species.id = animals.species_id 
+  WHERE specie_name='pokemon';
+
+SELECT * FROM OWNERS 
+  LEFT JOIN ANIMALS ON owners.id = animals.owner_id;
+
+SELECT specie_name, COUNT(*) FROM SPECIES 
+  LEFT JOIN ANIMALS ON species.id = animals.species_id 
+  GROUP BY SPECIE_NAME;
+
+SELECT * FROM OWNERS 
+  JOIN ANIMALS ON owners.id = animals.owner_id 
+  JOIN SPECIES ON animals.species_id = species.id 
+  WHERE owners.full_name='Jennifer Orwell' AND species.specie_name = 'digimon';
+
+SELECT full_name as owner_full_name, name as animal_name FROM OWNERS 
+  JOIN ANIMALS ON owners.id = animals.owner_id 
+  WHERE full_name='Dean Winchester' AND animals.escape_attempts = 0; 
+
+SELECT full_name, COUNT(*) as total_animals FROM OWNERS 
+  JOIN ANIMALS ON owners.id = animals.owner_id 
+  GROUP BY FULL_NAME
+  ORDER BY total_animals DESC;
